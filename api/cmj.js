baf67f4e99db591963a4fb028b84591623045296
sammurrayfit <sammurrayfit@gmail.com>
2026-05-05 21:35:56 -0400

    Update cmj.js

--- api/cmj.js
+++ api/cmj.js
@@ -2,7 +2,7 @@ const { fetchSheet } = require('./_sheet');
 module.exports = async (req, res) => {
   if (req.method === 'OPTIONS') return res.status(200).end();
   try {
-    const rows = await fetchSheet('VALD_CMJ');
+    const rows = await fetchSheet('CMJ_VALD');
     return res.status(200).json(Object.keys(rows[0] || {}));
   } catch (err) {
     res.status(500).json({ error: err.message });
